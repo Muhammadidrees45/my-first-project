5308e657acac6ce86e5aabb194106b83aea72198
M idress <midress1947@email.com>
2025-09-03 22:02:36 +0500

    first commit

---
+++ digital-clock/scripts.js
@@ -0,0 +1,13 @@
+const hour = document.getElementById("hour");
+const minut = document.getElementById("minut");
+const second = document.getElementById("second");
+
+
+
+setInterval(()=>{
+    let currentTime = new Date;
+    
+    hour.innerHTML = (currentTime.getHours()<10 ? "0" : "") + currentTime.getHours();
+    minut.innerHTML = (currentTime.getMinutes()<10?"0" : "") + currentTime.getMinutes();
+    second.innerHTML = (currentTime.getSeconds()<10?"0" : "") + currentTime.getSeconds();
+},1000)
